b122add1dc61ac4ca0efb689ec504e326f006d3d
Braydn Larsen <braydnl@gmail.com>
2024-07-26 10:36:23 -0700

    update to mainnet

--- public/js/global.js
+++ public/js/global.js
@@ -21,95 +21,33 @@ window.addEventListener("keplr_keystorechange", () => {
     location.reload(true);
 })
 async function connectKeplr() {
-    this.chainId = 'pulsar-3';
+    this.chainId = 'secret-4';  // Mainnet chain ID
 
-    // Keplr extension injects the offline signer that is compatible with cosmJS.
-    // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
-    // And it also injects the helper function to `window.keplr`.
-    // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
     if (!window.getOfflineSigner || !window.keplr) {
-        alert("Please install keplr extension");
+        alert("Please install the Keplr extension.");
     } else {
-        if (window.keplr.experimentalSuggestChain) {
-            try {
-                // Setup Secret Testnet (not needed on mainnet)
-                // Keplr v0.6.4 introduces an experimental feature that supports the feature to suggests the chain from a webpage.
-                // cosmoshub-3 is integrated to Keplr so the code should return without errors.
-                // The code below is not needed for cosmoshub-3, but may be helpful if you’re adding a custom chain.
-                // If the user approves, the chain will be added to the user's Keplr extension.
-                // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
-                // If the same chain id is already registered, it will resolve and not require the user interactions.
-                await window.keplr.experimentalSuggestChain({
-                    chainId: this.chainId,
-                    chainName: 'Secret Testnet',
-                    rpc: 'https://rpc.pulsar.scrttestnet.com',
-                    rest: "https://api.pulsar.scrttestnet.com",
-                    bip44: {
-                        coinType: 529,
-                    },
-                    coinType: 529,
-                    stakeCurrency: {
-                        coinDenom: 'SCRT',
-                        coinMinimalDenom: 'uscrt',
-                        coinDecimals: 6,
-                    },
-                    bech32Config: {
-                        bech32PrefixAccAddr: 'secret',
-                        bech32PrefixAccPub: 'secretpub',
-                        bech32PrefixValAddr: 'secretvaloper',
-                        bech32PrefixValPub: 'secretvaloperpub',
-                        bech32PrefixConsAddr: 'secretvalcons',
-                        bech32PrefixConsPub: 'secretvalconspub',
-                    },
-                    currencies: [
-                        {
-                            coinDenom: 'SCRT',
-                            coinMinimalDenom: 'uscrt',
-                            coinDecimals: 6,
-                        },
-                    ],
-                    feeCurrencies: [
-                        {
-                            coinDenom: 'SCRT',
-                            coinMinimalDenom: 'uscrt',
-                            coinDecimals: 6,
-                        },
-                    ],
-                    gasPriceStep: {
-                        low: 0.1,
-                        average: 0.25,
-                        high: 0.4,
-                    },
-                    features: ['secretwasm'],
-                });
+        try {
+            // Enable Keplr and check if the chain is already registered
+            await window.keplr.enable(this.chainId);
 
-                // This method will ask the user whether or not to allow access if they haven't visited this website.
-                // Also, it will request user to unlock the wallet if the wallet is locked.
-                // If you don't request enabling before usage, there is no guarantee that other methods will work.
-                await window.keplr.enable(this.chainId);
+            // @ts-ignore
+            const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(this.chainId);
+            const accounts = await keplrOfflineSigner.getAccounts();
+            
+            this.address = accounts[0].address;
 
-                // @ts-ignore
-                const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(this.chainId);
-                const accounts = await keplrOfflineSigner.getAccounts();
-                
-                this.address = accounts[0].address;
-
-                window.secretjs = new SecretNetworkClient({
-                  url: "https://api.pulsar.scrttestnet.com",
-                  chainId: this.chainId,
-                  wallet: keplrOfflineSigner,
-                  walletAddress: this.address,
-                  encryptionUtils: window.keplr.getEnigmaUtils(this.chainId),
-                });
-            } catch (error) {
-                console.error(error)
-            }
-        } else {
-            alert("Please use the recent version of keplr extension");
+            // Initialize the Secret Network client
+            window.secretjs = new SecretNetworkClient({
+                url: "https://lcd.mainnet.secretsaturn.net",
+                chainId: this.chainId,
+                wallet: keplrOfflineSigner,
+                walletAddress: this.address,
+                encryptionUtils: window.keplr.getEnigmaUtils(this.chainId),
+            });
+        } catch (error) {
+            console.error("Error connecting to Keplr:", error);
         }
     }
-
-
 
     if (this.address) {
         try {
@@ -118,30 +56,34 @@ async function connectKeplr() {
             document.querySelector("#wallet-name").innerHTML = wallet_name.name.slice(0,12);
             start();
         } catch (error) {
-            console.log(error);
+            console.log("Error getting wallet name:", error);
         }
     } else {
-        console.log("error connecting to keplr");
+        console.log("Error connecting to Keplr.");
     }
 }
 
+async function try_query_balance(viewing_key, contract, hash) {
+    try {
+        let tx = await window.secretjs.query.compute.queryContract({
+            contract_address: contract,
+            code_hash: hash,
+            query: {
+                balance: {
+                    address: window.secretjs.walletAddress,
+                    key: viewing_key,
+                    time: Date.now()
+                }
+            }
+        });
+        console.log(tx);
+        let snip_balance = tx.balance.amount / 1000000;
+        return snip_balance;
+    } catch (error) {
+        console.error("Error querying balance:", error);
+    }
+}
 
-async function try_query_balance(viewing_key, contract, hash){
-	let tx = await window.secretjs.query.compute.queryContract({
-	  contract_address: contract,
-	  code_hash: hash,
-	  query: {
-		  balance: {
-			  address: window.secretjs.address,
-			  key: viewing_key,
-			  time : Date.now()
-		  }
-	  }
-	});
-    console.log(tx);
-	snip_balance = tx.balance.amount / 1000000;
-	return(snip_balance);
-};
 
 function floorToDecimals(num, dec) {
     const multiplier = 10 ** dec;
@@ -227,9 +169,9 @@ if (window.innerWidth > 600) {
         sidebar.classList.remove("close");
     });
     homeSection.addEventListener("mouseover", () => {
-        sidebar.classList.add("close");
-        submenuList.classList.add('remove');
-        governanceItem.parentElement.classList.remove('open'); 
+        //sidebar.classList.add("close");
+        //submenuList.classList.add('remove');
+        //governanceItem.parentElement.classList.remove('open'); 
     });
 }
 
@@ -241,16 +183,6 @@ menuToggle.addEventListener('click', function() {
     } else {
         menuToggle.style.display = 'block';
     }
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    
-
-    governanceItem.addEventListener('click', function(event) {
-        event.preventDefault();
-        submenuList.classList.toggle('remove');
-        governanceItem.parentElement.classList.toggle('open'); 
-    });
 });
 
 
