1a6d5fa9a6c6c5b812928dd9bca5aba9d62bc295
Braydn Larsen <braydnl@gmail.com>
2025-01-11 01:09:30 -0800

    try try again

--- server/server.js
+++ server/server.js
@@ -302,7 +302,7 @@ app.all("/api/proxy/*", async (req, res) => {
 });
 
 // Also handle preflight for CORS
-app.options("/proxy/*", (req, res) => {
+app.options("api/proxy/*", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
