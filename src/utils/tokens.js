fc7e89e993f82d213bfe995632e1470ec3614ad8
Braydn Larsen <braydnl@gmail.com>
2025-03-06 18:03:15 -0800

    Update token details and remove FINA coin image

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -1,49 +1,46 @@
 // token details
 const tokens = {
-    ERTH: {
-        contract: "secret16snu3lt8k9u0xr54j2hqyhvwnx9my7kq7ay8lp",
-        hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
-        decimals: 6,
-        logo: "/images/logo.png"
+  ERTH: {
+    contract: "secret16snu3lt8k9u0xr54j2hqyhvwnx9my7kq7ay8lp",
+    hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
+    decimals: 6,
+    logo: "/images/coin/ERTH.png",
+  },
+  ANML: {
+    contract: "secret14p6dhjznntlzw0yysl7p6z069nk0skv5e9qjut",
+    hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
+    decimals: 6,
+    logo: "/images/coin/ANML.png",
+    lp: {
+      contract: "secret1cqxxq586zl6g5zly3536rc7crwfcmeluuwehvx",
+      hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
+      decimals: 6,
     },
-    ANML: {
-        contract: "secret14p6dhjznntlzw0yysl7p6z069nk0skv5e9qjut",
-        hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
-        decimals: 6,
-        logo: "/images/anml.png",
-        lp: {
-            contract: 'secret1cqxxq586zl6g5zly3536rc7crwfcmeluuwehvx',
-            hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
-            decimals: 6,
-        },
+  },
+  FINA: {
+    contract: "secret1s3z9xkpdsrhk86300tqnv6u466jmdmlegew2ve",
+    hash: "cfecd51a022c520c55429d974363fd7f065d20474af6a362da8737f73b7d9e80",
+    decimals: 6,
+    logo: "/images/coin/FINA.webp",
+    coingeckoId: "fina",
+    lp: {
+      contract: "secret1lzdf7fputcwun0cuapwurh7e6j2pzw8fyk046m",
+      hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
+      decimals: 6,
     },
-    FINA: {
-        contract: "secret1s3z9xkpdsrhk86300tqnv6u466jmdmlegew2ve",
-        hash: "cfecd51a022c520c55429d974363fd7f065d20474af6a362da8737f73b7d9e80",
-        decimals: 6,
-        logo: "/images/coin/FINA.webp",
-        coingeckoId: "fina",
-        lp: {
-            contract: 'secret1lzdf7fputcwun0cuapwurh7e6j2pzw8fyk046m',
-            hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
-            decimals: 6,
-        },
+  },
+  sSCRT: {
+    contract: "secret1k0jntykt7e4g3y88ltc60czgjuqdy4c9e8fzek",
+    hash: "af74387e276be8874f07bec3a87023ee49b0e7ebe08178c49d0a49c3c98ed60e",
+    decimals: 6,
+    logo: "/images/coin/SSCRT.png",
+    coingeckoId: "secret",
+    lp: {
+      contract: "secret1hk5lj62949s88q7kl892rkxyhxcxdpznzlw924",
+      hash: "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
+      decimals: 6,
     },
-    sSCRT: {
-        contract: "secret1k0jntykt7e4g3y88ltc60czgjuqdy4c9e8fzek",
-        hash: "af74387e276be8874f07bec3a87023ee49b0e7ebe08178c49d0a49c3c98ed60e",
-        decimals: 6,
-        logo: "/images/coin/SSCRT.png",
-        coingeckoId: "secret",
-        lp: {
-            contract: 'secret1hk5lj62949s88q7kl892rkxyhxcxdpznzlw924',
-            hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
-            decimals: 6,
-        },
-    },
+  },
 };
 
-
 export default tokens;
-
-
