5f5e44a8e9e9703f2eb6e368fef266abcc4b25e4
Braydn Larsen <braydnl@gmail.com>
2025-06-18 07:44:11 -0700

    Update SECRET_AI_URL in server.py to point to the new endpoint for improved service connectivity.

--- server/server.py
+++ server/server.py
@@ -42,7 +42,7 @@ REGISTRATION_CONTRACT = "secret12q72eas34u8fyg68k6wnerk2nd6l5gaqppld6p"
 REGISTRATION_HASH = "a31a5c5311f8e0d0a48116b11f349ca1ebb7be5d51bdecead3c79a66a2ab74d3"
 ANALYTICS_FILE = "analyticsData.json"
 # SECRET_AI_URL = "http://vm-jeeves-2.scrtlabs.com:11434" #CVM
-SECRET_AI_URL = "https://secretai-zqtr.scrtlabs.com:21434"
+SECRET_AI_URL = "https://secretai-rytn.scrtlabs.com:21434"
 ollama_client = Client(
     host=SECRET_AI_URL,
 )
