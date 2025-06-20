2f00efd1761355869b6c109515ea3f87ea8155b1
Braydn Larsen <braydnl@gmail.com>
2025-06-19 17:55:34 -0700

    Remove analytics manager functionality from server. This includes the deletion of the analytics_manager.py file, which contained logic for loading, saving, and updating analytics data, as well as related configurations in config.py. This change streamlines the server codebase by eliminating unused components.

--- server/config.py
+++ server/config.py
@@ -34,12 +34,6 @@ TOKENS = {
         "hash": "638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e",
         "decimals": 6,
     },
-    "FINA": {
-        "contract": "secret1s3z9xkpdsrhk86300tqnv6u466jmdmlegew2ve",
-        "hash": "cfecd51a022c520c55429d974363fd7f065d20474af6a362da8737f73b7d9e80",
-        "decimals": 6,
-        "coingeckoId": "fina",
-    },
     "sSCRT": {
         "contract": "secret1k0jntykt7e4g3y88ltc60czgjuqdy4c9e8fzek",
         "hash": "af74387e276be8874f07bec3a87023ee49b0e7ebe08178c49d0a49c3c98ed60e",
